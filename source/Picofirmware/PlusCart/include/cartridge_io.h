f8e55e20a8f7cf0bff3733d9790b63c0db74abce
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-03-26 14:43:48 +0100

    define new hardware type PicoCart

--- source/Picofirmware/PlusCart/include/cartridge_io.h
+++ source/Picofirmware/PlusCart/include/cartridge_io.h
@@ -11,16 +11,9 @@
 #define PINENABLE  14
 #define PINROMDATA 15
 
-#define ADDRWIDTH 12
-#define DATAWIDTH 8
-
-const uint32_t addr_gpio_mask = (0xFFF << PINROMADDR);
-const uint32_t a12_gpio_mask = (0x1 << PINENABLE);
-const uint32_t data_gpio_mask = (0xFF << PINROMDATA);
-
-#define ADDR_GPIO_MASK  addr_gpio_mask
-#define A12_GPIO_MASK   a12_gpio_mask
-#define DATA_GPIO_MASK  data_gpio_mask
+#define ADDR_GPIO_MASK  (0xFFF << PINROMADDR)
+#define A12_GPIO_MASK   (0x1 << PINENABLE)
+#define DATA_GPIO_MASK  (0xFF << PINROMDATA)
 
 #define ADDR_IN (sio_hw->gpio_in & (ADDR_GPIO_MASK | A12_GPIO_MASK)) >> PINROMADDR
 #define DATA_OUT(v) sio_hw->gpio_togl = (sio_hw->gpio_out ^ (v << PINROMDATA)) & DATA_GPIO_MASK
@@ -29,6 +22,7 @@ const uint32_t data_gpio_mask = (0xFF << PINROMDATA);
 
 #define SET_DATA_MODE_IN    sio_hw->gpio_oe_clr = DATA_GPIO_MASK;
 #define SET_DATA_MODE_OUT   sio_hw->gpio_oe_set = DATA_GPIO_MASK;
+#define SET_DATA_MODE(m)    sio_hw->gpio_oe_togl = (sio_hw->gpio_oe ^ (m << PINROMDATA));
 
 // Used to control exit function
 extern uint16_t EXIT_SWCHB_ADDR;
